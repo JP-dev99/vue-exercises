c1516849e56a9a28a3414980a5820a4ef0b5a6d4
João Pedro G <joaocitimo@gmail.com>
2021-11-03 14:30:37 -0300

    v-for_li

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <ul>
+      <li v-for="photo in photos">
+        <img :src="photo.url" :alt="photo.title">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'alurapic',
+      photos: [
+        {
+        url: 'https://s.conjur.com.br/img/b/cachorro-buldogue-frances.jpeg',
+        title: 'dog'
+        },
+        {
+        url: 'https://www.petz.com.br/blog/wp-content/uploads/2020/08/cat-sitter-felino.jpg',
+        title: 'cat'
+        }
+      ]
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
