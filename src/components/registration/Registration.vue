525c3bf92fe9df031cc9c7b605540a7b845431f0
João Pedro G <joaocitimo@gmail.com>
2021-11-08 14:32:53 -0300

    add_home_component

---
+++ src/components/registration/Registration.vue
@@ -0,0 +1,15 @@
+<template>
+    <h2 class="center">Cadastro</h2>
+</template>
+
+<script lang="ts">
+export default {
+    
+}
+</script>
+
+<style scoped>
+.center {
+    text-align: center;
+}
+</style>
